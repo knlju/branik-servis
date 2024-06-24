03d0b50c9214a61bcfff32febb7baa8cfc651529
knlju <ljubisakn@gmail.com>
2024-06-24 18:52:21 +0200

    shuffle

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-let BASE = 'branik-servis';
-let SITE = 'https://knlju.github.io/';
+let BASE = '/branik-servis';
+let SITE = 'https://knlju.github.io';
 
 if (import.meta.env.DEV) {
   BASE = '/';
