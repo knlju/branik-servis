eff8e9a9735117e0d0a53600d325613b58aa1b94
knlju <ljubisakn@gmail.com>
2024-06-24 21:15:37 +0200

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
+import relativeLinks from "astro-relative-links";
 let BASE = '/branik-servis';
 let SITE = 'https://knlju.github.io';
-
 if (import.meta.env.DEV) {
-  BASE = '/';
-  SITE = 'http://localhost:3000/';
+  BASE = '/branik-servis';
+  SITE = 'http://localhost:3000';
 }
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), relativeLinks()],
   site: SITE,
   base: BASE,
   output: "static"
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,20 +12,20 @@ export default function Header() {
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+            <span className="sr-only">Auto servis Kastratović</span>
+            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="auto servis kastratovis logo" />
           </a>
         </div>
         <div className="flex lg:hidden">
           <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" onClick={toggleMenu}>
-            <span className="sr-only">Open main menu</span>
+            <span className="sr-only">Otvori meni auto servisa</span>
             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
           </button>
         </div>
         <div className="hidden lg:flex lg:justify-end gap-5">
-          <a href="#about" className="text-sm font-semibold leading-6 text-gray-900">O Servisu</a>
+          <a href="#services" className="text-sm font-semibold leading-6 text-gray-900">Usluge</a>
           <a href="#gallery" className="text-sm font-semibold leading-6 text-gray-900">Galerija</a>
           <a href="#contact" className="text-sm font-semibold leading-6 text-gray-900">Kontakt</a>
         </div>
@@ -36,11 +36,11 @@ export default function Header() {
           <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                <span className="sr-only">Auto servis Kastratovic</span>
+                <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="auto servis kastratovic logo" />
               </a>
               <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700" onClick={toggleMenu}>
-                <span className="sr-only">Close menu</span>
+                <span className="sr-only">Zatvori meni auto servisa</span>
                 <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
@@ -50,7 +50,7 @@ export default function Header() {
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="py-6">
                   <div className="space-y-2 py-6">
-                    <a href="#about" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" onClick={toggleMenu}>O Servisu</a>
+                    <a href="#services" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" onClick={toggleMenu}>Usluge</a>
                     <a href="#gallery" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" onClick={toggleMenu}>Galerija</a>
                     <a href="#contact" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" onClick={toggleMenu}>Kontakt</a>
                   </div>
